c64a1e90d5038e4d090f41df0f5fcb526ab18696
jsmithBadin <135791484+jsmithBadin@users.noreply.github.com>
2024-10-07 14:24:36 -0400

    added instructions to etchASketch javascript file

--- etchASketch.js
+++ etchASketch.js
@@ -1,2 +1,8 @@
 //This will be the javaScript file that runs my scripting for the Etch-A-Sketch project
-console.log("2"+3);
+
+// Add a 16 x 16 array of divs to the mainBoard
+
+// Add hover effect (when cursor enters div, change div background color)
+
+// Add a button that prompts the user for a new canvas size 
+// (i.e. make a new nxn div board in the same mainBoard size)
