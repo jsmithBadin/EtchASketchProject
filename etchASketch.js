785c6fda610779121a1aeacbf51c66ca6b2eaab7
jsmithBadin <135791484+jsmithBadin@users.noreply.github.com>
2025-01-15 10:36:05 -0500

    added alorithm to add and class tiles within each row

--- etchASketch.js
+++ etchASketch.js
@@ -7,10 +7,13 @@ function makeBoard(size = 16){
         let j = 0;
         const row = document.createElement("div");
         row.classList.add("row");
-        row.textContent = i;
         mainBoard.appendChild(row);
         // boardTile.setAttribute.("id", "row");
         while (j < size){
+            const tile = document.createElement("div");
+            tile.classList.add("tile");
+            tile.textContent = i * 100 + j;
+            row.appendChild(tile);
             j += 1;
         }
         i += 1;
