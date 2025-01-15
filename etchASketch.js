ed9a3cd3625ba414554ca75cb40cc3fa4a6cb9ea
jsmithBadin <135791484+jsmithBadin@users.noreply.github.com>
2025-01-15 09:11:20 -0500

    added algorithm to create rows inside main board

--- etchASketch.js
+++ etchASketch.js
@@ -1,5 +1,24 @@
 //This will be the javaScript file that runs my scripting for the Etch-A-Sketch project
+function makeBoard(size = 16){
+    const mainBoard = document.querySelector("#mainBoard");
+    let i = 0;
+    console.log(`i = ${i}`);
+    while (i < size){
+        let j = 0;
+        const row = document.createElement("div");
+        row.classList.add("row");
+        row.textContent = i;
+        mainBoard.appendChild(row);
+        // boardTile.setAttribute.("id", "row");
+        while (j < size){
+            j += 1;
+        }
+        i += 1;
+        console.log(`Printing row ${i}`);
+    }
+}
 
+makeBoard();
 // Add a 16 x 16 array of divs to the mainBoard
 
 // Add hover effect (when cursor enters div, change div background color)
