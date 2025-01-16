fc860766806eee820fbf67a3a2dc50f921429036
jsmithBadin <135791484+jsmithBadin@users.noreply.github.com>
2025-01-16 12:28:31 -0500

    fixed resetTriggered function to clear old board, make a new board, and reapply eventListeners

--- etchASketch.js
+++ etchASketch.js
@@ -1,4 +1,5 @@
 //This will be the javaScript file that runs my scripting for the Etch-A-Sketch project
+// Add a 16 x 16 array of divs to the mainBoard ----------- DONE
 function makeBoard(size = 16){
     const mainBoard = document.querySelector("#mainBoard");
     let i = 0;
@@ -11,7 +12,7 @@ function makeBoard(size = 16){
         while (j < size){
             const tile = document.createElement("div");
             tile.classList.add("tile");
-            //tile.style.height()
+            //tile.style.height(38)
             row.appendChild(tile);
             j += 1;
         }
@@ -21,25 +22,31 @@ function makeBoard(size = 16){
 }
 
 function resetTriggered(size){
-    if (size > 100 || !(Number.isInteger(size))) {
+    if (size > 100 || (isNaN(size))) {
         alert("Maximum size is 100, please enter a new size value as a whole number using digits");
     }
     else {
+        document.querySelectorAll(".tile").forEach(elem => elem.remove())
+        document.querySelectorAll(".row").forEach(elem => elem.remove())
         makeBoard(size);
+        document.querySelectorAll(".tile").forEach(elem => elem.addEventListener("mouseenter", function(t) {
+            t.target.style.background = "#44cc44";
+        }))
+        
     }
 }
 
 makeBoard();
 
+// Add hover effect (when cursor enters div, change div background color) ---------------- DONE
 // add event listeners.  Because we're adding to ALL elements of a class, we need to use querySelectorAll AND
 // forEach in combination
 document.querySelectorAll(".tile").forEach(elem => elem.addEventListener("mouseenter", function(t) {
     t.target.style.background = "#44cc44";
 }))
 
-// Add a 16 x 16 array of divs to the mainBoard ----------- DONE
 
-// Add hover effect (when cursor enters div, change div background color)
-
-// Add a button that prompts the user for a new canvas size 
+// Add a button that prompts the user for a new canvas size  
 // (i.e. make a new nxn div board in the same mainBoard size)
+const btn = document.querySelector(".reset");
+btn.addEventListener("click", () => resetTriggered(prompt("How many squares should the grid hold?  Please enter the number of rows or columns you would like:")));
